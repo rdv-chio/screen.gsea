d524f5f4df9ec0ca9159569de000fd04262e7c54
Nick Neill <njneill@bcm.edu>
2015-06-19 10:42:16 -0500

    added functionality
    added update annotation and use mean/max effect

--- filter_genelist.r
+++ filter_genelist.r
@@ -1,20 +1,54 @@
-## to filter a gene list based on pval and assign one (max effect) hairpin per gene
-## columns of input should be: shrna, gene, effect, pval
+## to filter a gene list based on pval and assign one effect value per gene
 
-filter_genelist = function(input,pval_threshold,output) {
+filter_genelist = function(input,annotation_file,pval_threshold,absolute_effects="FALSE",effect_type="max",output) {
 
-## read dataset and filter based on pval_threshold
+## input: Input filename. Columns of input should be: shrna, gene, effect, pval.
+## annotation_file: File containing current gene annotations for each hairpin. Columns should be: shrna, gene.
+## pval_threshold: Numeric 0-1. Hairpins with p-values greater than this threshold will be removed from the dataset.
+## absolute_effects: Logical. If "TRUE", all effects will be converted to positive values. 
+## effect_type: "mean" or "max". Determines whether mean or maximum effect should be used for each gene.
+## output: Output filename.
 
-	data = read.table(input,header=T,sep='\t',colClasses=c('character','factor','numeric','numeric'))
-	data = data[data[[4]]<pval_threshold,]
+## read dataset
+
+	data = read.table(input,header=T,sep='\t',colClasses=c('character','character','numeric','numeric'))
 	
-## remove genes with hairpins with both positive and negative effects
+## update shRNA annotations
+	
+	annotation_file = read.table(annotation_file,header=T,sep='\t',colClasses=c('character','character'))
+	annotations = vector('character',length=length(data[[1]]))
+	for(i in 1:length(data[[1]])) {
+		annotations[i] = annotation_file[annotation_file[[1]] == data[[1]][i],2]
+	}
+	data = data.frame(cbind(data,annotations),stringsAsFactors=F)
+	data = data[,c(1,5,3,4)]
+	data[[3]] = as.numeric(data[[3]])
+	data[[4]] = as.numeric(data[[4]])
+	colnames(data) = c('shRNA','Gene','Effect','P Value')
 
-	genes = levels(data[[2]])
+## filter based on pval_threshold
+	
+	data = data[data[[4]]<=pval_threshold,]
+	
+## calculate mean effect for each gene
+
+	genes = unique(data[[2]])
+	mean.effects = vector('numeric',length(genes))
+	for(i in 1:length(genes)) {
+		effects = data[data[[2]] == genes[i],3]
+		mean.effects[i] = mean(effects)
+	}
+	if(absolute == "TRUE") {
+		mean.effects = abs(mean.effects)
+	} else {
+	}
+		
+## calculate max effect for each gene (genes with both positive and negative effects are removed)
+
 	both.indicator = vector('numeric',length(genes))
 	max.effects = vector('numeric',length(genes))
 	min.effects = vector('numeric',length(genes))
-	for(i in 1:length(both.indicator)) {
+	for(i in 1:length(genes)) {
 		effects = data[data[[2]] == genes[i],3]
 		max.effects[i] = max(effects)
 		min.effects[i] = min(effects)
@@ -23,23 +57,35 @@ filter_genelist = function(input,pval_threshold,output) {
 		}
 	}
 	new.genes = genes[both.indicator != 1]
-	
-## find max effect for each gene
-	
 	new.max.effects = max.effects[both.indicator != 1]
 	new.min.effects = min.effects[both.indicator != 1]
 	abs.min.effects = abs(new.min.effects)
-	final.effects = vector('numeric',length(new.genes))
-	for(i in 1:length(final.effects)) {
+	final.max.effects = vector('numeric',length(new.genes))
+	for(i in 1:length(final.max.effects)) {
 		if(new.max.effects[i]>=abs.min.effects[i]) {
-			final.effects[i] = new.max.effects[i]
+			final.max.effects[i] = new.max.effects[i]
+		} else if(absolute == "TRUE") {
+			final.max.effects[i] = abs.min.effects[i]
 		} else {
-			final.effects[i] = new.min.effects[i]
+			final.max.effects[i] = new.min.effects[i]
 		}
 	}
-	new.data = data.frame(cbind(new.genes,max.effects),stringsAsFactors=F)
+	
+## write output
+
+	if(effect_type == "mean") {
+		final.genes = genes
+		final.effects = mean.effects
+	} else if(effect_type == "max") {
+		final.genes = new.genes
+		final.effects = final.max.effects
+	} else {
+		stop("Invalid effect_type.")
+	}
+	final.effects[final.effects==0] = min(final.effects)
+	new.data = data.frame(cbind(final.genes,final.effects),stringsAsFactors=F)
 	new.data[[2]] = as.numeric(new.data[[2]])
-	colnames(new.data) = c('Gene','Max Effect')
+	colnames(new.data) = c('Gene','Effect')
 	new.data = new.data[order(new.data[[2]],decreasing=T),]
 	write.table(new.data,output,row.names=F,sep='\t')
 }
